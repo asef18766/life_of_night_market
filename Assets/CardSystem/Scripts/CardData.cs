873597d5ba72da5ee5b6869565e6a0ed68cabda9
BCatw <memo123449@gmail.com>
2019-04-13 18:34:06 +0800

    CardSystem

---
+++ Assets/CardSystem/Scripts/CardData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "CD",menuName = "CardData")]
+
+public class CardData : ScriptableObject
+{
+    public int int_Value_;
+    public float f_Risk_;
+    public Sprite sprite_CardArt_;
+}
---
+++ Assets/CardSystem/Scripts/CardSystem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSystem : MonoBehaviour
+{
+
+    public GameObject gObj_Yamafuda_;
+    public GameObject gObj_Tefuda_;
+    public GameObject Prefab_Card_;
+    public int[] i_PlayerCardNumber_;
+    public int i_thisTimeYouDraw_;
+    public CardDataPool m_CDP_;
+
+    void Start()
+    {
+        gObj_Yamafuda_ = GameObject.Find("Yamafuda");
+        gObj_Tefuda_ = GameObject.Find("Tefuda");
+}
+
+    public void DrawCard()
+    {
+        int i = m_CDP_.CDPool_.ToArray().Length;
+        i_thisTimeYouDraw_ = Random.Range(0, i);
+        GameObject NewCard_ = Instantiate(Prefab_Card_,gObj_Tefuda_.transform);
+        NewCard_.GetComponent<CardRefer>().PrintCard(i_thisTimeYouDraw_,m_CDP_);
+
+        gObj_Yamafuda_.SetActive(false);
+    }
+
+    public void ShowTefuda(string PlayerID)
+    {
+        gObj_Tefuda_.SetActive(true);
+    }
+
+    public void UsingCard()
+    {
+
+    }
+
+    public void IfContinue(bool b_TorF_)
+    {
+        if (b_TorF_)
+        {
+            //Check();
+            ShowYamafuda();
+        }
+        else if(!b_TorF_)
+        {
+            gObj_Tefuda_.SetActive(false);
+            //Dice();
+        }
+    }
+    
+    public void ShowYamafuda()
+    {
+        gObj_Tefuda_.SetActive(true);
+    }
+
+}
