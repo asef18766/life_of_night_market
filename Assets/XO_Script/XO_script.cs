f276d5bceae5622fb131756feb5610c91b7f7014
xo0821ms <49581796+xo0821ms@users.noreply.github.com>
2019-04-13 23:00:46 +0800

    float[] getPlayerTotalEarn()

---
+++ Assets/XO_Script/XO_script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XO_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    float[] getPlayerTotalEarn()
+    {
+        float[] money = new float[4];
+        Land_[] _land = FindObjectsOfType<Land_>();
+        for (int u = 0; u != 4; ++u)
+            money[u] = 0.0f;
+        for (int u = 0; u != _land.Length; ++u)
+        {
+            DeltaMoney_[] land_ret = _land[u].gain_profit();
+            for (int i = 0; i != land_ret.Length; ++i)
+                money[land_ret[i].owner_id - 1] += land_ret[i].amount;
+        }
+        return money;
+    }
+
+
+
+}
