b4359acd8107f260b78332617efae7d5d1644f53
Hayato <hayatos@gmail.com>
2019-04-14 03:06:49 +0800

    Update WorldEvent_.cs
    手動將被我覆蓋的WorldEvent_給塞回去...

--- Assets/WorldEventSystem/WorldEvent_.cs
+++ Assets/WorldEventSystem/WorldEvent_.cs
@@ -1,56 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class WorldEvent_:MonoBehaviour
+using UnityEngine.UI;
+public class WorldEvent_ : MonoBehaviour
 {
-    const float add_rate=0.2f;
-    float[] add_rate_sep=new float[]{0.7f,0.2f,0.1f};
+    const float add_rate = 0.2f;
+    float[] add_rate_sep = new float[] { 0.7f, 0.2f, 0.1f };
 
-    List<I_Event_> event_pool;
+    List<I_Event_> event_pool = new List<I_Event_>();
     void Start()
     {
         //initialize addcard events
-        AddCards_[] _add_events=new AddCards_[3];
-        for(int u=0;u!=3;++u)
+        AddCards_[] _add_events = new AddCards_[3];
+        for (int u = 0; u != 3; ++u)
         {
-            _add_events[u].pro=add_rate*add_rate_sep[u];
-            _add_events[u].AddCardAmount=u+1;
+            _add_events[u] = new AddCards_();
+            _add_events[u].pro = add_rate * add_rate_sep[u];
+            _add_events[u].AddCardAmount = u + 1;
             event_pool.Add(_add_events[u]);
         }
 
         //initialize add destory rate events
-        AddRate_ _adr_event=new AddRate_();
-        _adr_event.pro=0.7f;
+        AddRate_ _adr_event = new AddRate_();
+        _adr_event.pro = 0.7f;
         event_pool.Add(_adr_event);
 
         //initialize Destory house under rate event
-        DestoryHouseUnderRate_ _dhur_event=new DestoryHouseUnderRate_();
-        _dhur_event.pro=0.05f;
+        DestoryHouseUnderRate_ _dhur_event = new DestoryHouseUnderRate_();
+        _dhur_event.pro = 0.05f;
         event_pool.Add(_dhur_event);
 
         //initialize Destory house type event
-        DestoryHouseType_ _dht_event=new DestoryHouseType_();
-        _dht_event.pro=0.05f;
+        DestoryHouseType_ _dht_event = new DestoryHouseType_();
+        _dht_event.pro = 0.05f;
         event_pool.Add(_dht_event);
     }
     I_Event_ getEvent()
     {
-        float choice=Random.Range(0.0f,1.0f);
-        float cur=0.0f;
-        for(int u=0;u!=event_pool.Count;++u)
+        float choice = Random.Range(0.0f, 1.0f);
+        float cur = 0.0f;
+        for (int u = 0; u != event_pool.Count; ++u)
         {
-            if(cur<=choice&&choice<cur+event_pool[u].pro)
+            if (cur <= choice && choice < cur + event_pool[u].pro)
                 return event_pool[u];
+            cur += event_pool[u].pro;
         }
         return null;
     }
-    string generateEvent()
+    public string generateEvent()
     {
-        CardSystem cs=FindObjectOfType<CardSystem>();
-        Land_[] lands=FindObjectsOfType<Land_>();
-        I_Event_ _eve=getEvent();
-        _eve.act(cs,lands);
+        CardSystem cs = FindObjectOfType<CardSystem>();
+        Land_[] lands = FindObjectsOfType<Land_>();
+        I_Event_ _eve = getEvent();
+        _eve.act(cs, lands);
         return _eve.event_msg;
+    }
+    IEnumerator UpdateText(float delaytime)
+    {
+        timelock = true;
+        string eve = generateEvent();
+        GetComponent<Text>().text = eve;
+        Debug.Log("update!!");
+        yield return new WaitForSeconds(delaytime);
+        timelock = false;
+    }
+    bool timelock = false;
+    void Update()
+    {
+        if (timelock == false)
+            StartCoroutine(UpdateText(1));
+
     }
 }
