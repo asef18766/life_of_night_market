95e7bd6df6da595fb2e0775ad3be9378f586de67
BCatw <memo123449@gmail.com>
2019-04-13 21:25:49 +0800

    [DaCatw]Update Card

--- Assets/CardSystem/Scripts/CardRefer.cs
+++ Assets/CardSystem/Scripts/CardRefer.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class CardRefer : MonoBehaviour
+public class CardRefer : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
 {
+    public GameObject gObj_BeingDrag_;
+    public Vector2 v2_StartPosit_;
+
     public Image UI_Image_Picture_;
     public Text UI_Text_Value_;
     public Text UI_Text_Risk_;
@@ -12,12 +16,37 @@ public class CardRefer : MonoBehaviour
     public void PrintCard(int CardNumber, CardDataPool m_CardDataPool_)
     {
         UI_Image_Picture_ = transform.GetChild(0).GetComponent<Image>();
-        UI_Text_Value_ = transform.GetChild(1).GetComponent<Text>();
-        UI_Text_Risk_ = transform.GetChild(2).GetComponent<Text>();
+        UI_Text_Value_ = transform.GetChild(3).GetComponent<Text>();
+        UI_Text_Risk_ = transform.GetChild(4).GetComponent<Text>();
 
         UI_Image_Picture_.sprite = m_CardDataPool_.CDPool_[CardNumber].sprite_CardArt_;
         UI_Text_Value_.text = ""+ m_CardDataPool_.CDPool_[CardNumber].int_Value_;
         UI_Text_Risk_.text = m_CardDataPool_.CDPool_[CardNumber].f_Risk_ + "%";
     }
+
+    public void OnMouseDown()
+    {
+        transform.position = Input.mousePosition;
+        Debug.Log("AA");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        gObj_BeingDrag_ = gameObject;
+        v2_StartPosit_ = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        gObj_BeingDrag_ = null;
+        v2_StartPosit_ = new Vector2(0, 0);
+    }
+
 
 }
---
+++ Assets/CardSystem/Scripts/Tefuda.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tefuda : MonoBehaviour
+{
+    public float f_CardInter_;
+    public GameObject prefab_Card_;
+    public int i_TefudaCountNow_;
+    public int i_TefudaCountLast_;
+
+    void Start()
+    {
+        f_CardInter_ = prefab_Card_.transform.GetChild(1).GetComponent<Image>().rectTransform.sizeDelta.x /4;
+    }
+
+    void Update()
+    {
+        i_TefudaCountNow_ = transform.childCount;
+        if(i_TefudaCountLast_ != i_TefudaCountNow_)
+        {
+            //RefreshCardPosit();
+        }
+    }
+
+    void ShowUp()
+    {
+
+    }
+
+    void RefreshCardPosit()
+    {
+        if (i_TefudaCountNow_ % 2 == 0)
+        {
+            for (int i = 0; i < i_TefudaCountNow_; i++)
+            {
+                //transform.GetChild(i).transform.position = new Vector2((1+Mathf.Round(i/2))*(-1^i)*f_CardInter_, 0);
+                transform.GetChild(i).GetComponent<RectTransform>().position = new Vector2((1 + Mathf.Round(i / 2)) * (-1 ^ i) * f_CardInter_, 0);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < i_TefudaCountNow_; i++)
+            {
+                transform.GetChild(i).transform.position = new Vector2((Mathf.Round(i / 2)) * (-1 ^ i) * f_CardInter_, 0);
+            }
+        }
+        i_TefudaCountLast_ = i_TefudaCountNow_;
+    }
+}
