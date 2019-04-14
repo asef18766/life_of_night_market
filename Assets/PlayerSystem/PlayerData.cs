4016c588ae4a965d677e384d804a5c73911a2c96
Hayato <hayatos@gmail.com>
2019-04-14 11:45:15 +0800

    在Player場景中新增 [PlayerData] 物件 修改裡面的玩家金錢就會反應到UI上

---
+++ Assets/PlayerSystem/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public float money_P1;
+    public float money_P2;
+    public float money_P3;
+    public float money_P4;
+
+}
